886d885454cb7723b0dcad471ff8e2fd92ada46d
dougiemath <douglasimatheson@gmail.com>
2021-10-18 20:16:52 +0000

    added nationality to add_student, appended to list

--- run.py
+++ run.py
@@ -15,19 +15,31 @@ SHEET = GSPREAD_CLIENT.open('SchoolDatabase')
 STUDENT_DATA = SHEET.worksheet("studentdata")
 
 def add_new_student():
+    student_details = []
     while True:
         family_name = input("Please enter the student's family name: ")
         validate_data(family_name)
-        
+       
         if validate_data(family_name):
+            student_details.append(family_name)
             break
     while True:
         first_name = input("Please enter the student's first name: ")
         validate_data(first_name)
-        
+       
         if validate_data(first_name):
+            student_details.append(first_name)
             break
     
+    while True:
+        nationality = input("Please enter the student's nationality: ")
+        validate_data(nationality)
+       
+        if validate_data(nationality):
+            student_details.append(nationality)
+            break
+
+    print(student_details)
 
 def validate_data(values):
     try:
