01726110ed98f972e187bcea5b7fed3fde3a6831
dougiemath <douglasimatheson@gmail.com>
2021-10-18 15:40:26 +0000

    Added validation using isalpha for student name

--- run.py
+++ run.py
@@ -11,4 +11,22 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('SchoolDatabase')
+STUDENT_DATA = SHEET.worksheet("studentdata")
 
+def add_new_student():
+    family_name = input("Please enter the student's family name: ")
+    validate_data(family_name)
+
+def validate_data(values):
+    print(values)
+    try:
+        if values.isalpha() == False:
+            raise ValueError(
+                    "Try again"
+                )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
+
+
+add_new_student()
