2306825ad889a9f7308fc55bfdbcab4cb65cf615
dougiemath <douglasimatheson@gmail.com>
2021-10-18 12:26:52 +0000

    added While loop to add_student funct

--- run.py
+++ run.py
@@ -45,42 +45,46 @@ def start():
             print("Invalid choice, please enter a number 1-3")
 
 def add_student():
-    add_student_data = []
-    print("""You will be asked to input the following data:
-    Family Name, First Name(s), Email Address, Age, Nationality, Course Start/End Date, and Test Results\n""")
-    family_name = input("Please Enter the student's family name: ")
-    first_name = input("Please Enter the student's first name(s): ")
-    email_address = input("Please Enter the student's email: ")
-    student_age = input("Please Enter the student's age: ")
-    student_nationality = input("Please Enter the student's nationality: ")
-    student_course_start = input("Please Enter the student's course start date: ")
-    student_course_end = input("Please Enter the student's course end date: ")
-    student_test_results = input("Please Enter the student's course test results: ")
-    print("\nYou have entered the following details:\n")
-    print(f"Family name: {family_name}.")
-    print(f"First name(s): {first_name}.")
-    print(f"Email address: {email_address}.")
-    print(f"Age: {student_age}.")
-    print(f"Nationality: {student_nationality}.")
-    print(f"Starting date of course: {student_course_start}.")
-    print(f"End date of course: {student_course_end}.")
-    print(f"Test results {student_test_results}.")
-        
-    confirmation = input("\nIs the data correct? Y/N ")
-    if confirmation == "y" or confirmation =="Y":
-        add_student_data.append(family_name)
-        add_student_data.append(first_name)
-        add_student_data.append(email_address)
-        add_student_data.append(student_age)
-        add_student_data.append(student_nationality)
-        add_student_data.append(student_course_start)
-        add_student_data.append(student_course_end)
-        add_student_data.append(student_test_results)
-    print(add_student_data)
-    print("accessing sheet")
-    student_data = SHEET.worksheet("studentdata")
-    student_data.append_row(add_student_data)
-    print("sheet updated")
+    while True:
+        add_student_data = []
+        print("""You will be asked to input the following data:
+        Family Name, First Name(s), Email Address, Age, Nationality, Course Start/End Date, and Test Results\n""")
+        family_name = input("Please Enter the student's family name: ")
+        first_name = input("Please Enter the student's first name(s): ")
+        email_address = input("Please Enter the student's email: ")
+        student_age = input("Please Enter the student's age: ")
+        student_nationality = input("Please Enter the student's nationality: ")
+        student_course_start = input("Please Enter the student's course start date: ")
+        student_course_end = input("Please Enter the student's course end date: ")
+        student_test_results = input("Please Enter the student's course test results: ")
+        print("\nYou have entered the following details:\n")
+        print(f"Family name: {family_name}.")
+        print(f"First name(s): {first_name}.")
+        print(f"Email address: {email_address}.")
+        print(f"Age: {student_age}.")
+        print(f"Nationality: {student_nationality}.")
+        print(f"Starting date of course: {student_course_start}.")
+        print(f"End date of course: {student_course_end}.")
+        print(f"Test results {student_test_results}.")
+            
+        confirmation = input("\nIs the data correct? Y/N ")
+        if confirmation == "y" or confirmation =="Y":
+            add_student_data.append(family_name)
+            add_student_data.append(first_name)
+            add_student_data.append(email_address)
+            add_student_data.append(student_age)
+            add_student_data.append(student_nationality)
+            add_student_data.append(student_course_start)
+            add_student_data.append(student_course_end)
+            add_student_data.append(student_test_results)
+            print(add_student_data)
+            print("accessing sheet")
+            student_data = SHEET.worksheet("studentdata")
+            student_data.append_row(add_student_data)
+            print("sheet updated")
+            break
+        else:
+            break
 
 
 def search_student():
